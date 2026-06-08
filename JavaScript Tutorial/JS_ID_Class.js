9bae505fcdd2230ea0dce0b05946f1488f57029b
Sayanjit <sayanjitjana373@gmail.com>
2026-06-08 19:12:19 +0530

    JavaScript - Selecting by Ids, Classes Code Updated

--- JavaScript Tutorial/JS_ID_Class.js
+++ JavaScript Tutorial/JS_ID_Class.js
@@ -1,15 +1,15 @@
-console.log("Harry")
+console.log("Sayanjit")
 
-// let boxes = document.getElementsByClassName("box")
-// console.log(boxes)
+let boxes = document.getElementsByClassName("box")
+console.log(boxes)
 
-// boxes[2].style.backgroundColor = "red"
+boxes[2].style.backgroundColor = "red"
 
-// document.getElementById("redbox").style.backgroundColor = "red"
+document.getElementById("redbox").style.backgroundColor = "red" // It is used for global ID and it can not be used for multiple elements. It is used to select a single element by its ID.
 
-// document.querySelector(".box").style.backgroundColor = "green";
-console.log(document.querySelectorAll(".box"))
+// document.querySelector(".box").style.backgroundColor = "green"; // It is used to select the first element with the class "box" and change its background color to green.
+// console.log(document.querySelectorAll(".box"))
 
-document.querySelectorAll(".box").forEach(e =>{
+document.querySelectorAll(".box").forEach(e =>{ // It is used to select all elements with the class "box" and change their background color to green.`
     e.style.backgroundColor = "green";
 }) 
