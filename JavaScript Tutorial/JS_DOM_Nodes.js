28d0a526d65d9adeb3bd1171da0cb015cc4fefab
Sayanjit <sayanjitjana373@gmail.com>
2026-06-08 14:17:38 +0530

    JavaScript DOM - Children, Parent & Sibling Nodes Code Update

--- JavaScript Tutorial/JS_DOM_Nodes.js
+++ JavaScript Tutorial/JS_DOM_Nodes.js
@@ -1,5 +1,5 @@
 console.log("Hello world")
 
-document.body.firstElementChild 
-document.body.firstElementChild.childNodes 
-document.body.firstElementChild.children
+document.body.firstElementChild // returns the first child element of the body, which is usually the <h1> element in this case
+document.body.firstElementChild.childNodes // returns all the child nodes, including text nodes (like whitespace) , comment nodes, and element nodes
+document.body.firstElementChild.children // returns only the element nodes
