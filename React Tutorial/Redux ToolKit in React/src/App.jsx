0e8bad0b8baf25db81456ae4dd02d9eb687dcc96
Sayanjit Jana <sayanjitjana373@gmail.com>
2026-07-21 11:39:22 +0530

    Redux ToolKit in React

---
+++ React Tutorial/Redux ToolKit in React/src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from './components/Navbar'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment, multiply } from './redux/counter/counterSlice'
+
+function App() { 
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+    <>
+      <Navbar />
+      <div>
+        <button onClick={() => dispatch(decrement())}>-</button>
+        Currently count is {count}
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(multiply())}>*</button>
+      </div>
+
+    </>
+  )
+}
+
+export default App
